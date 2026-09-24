2586b12dbcc0a7241ec56484f5e167fce59b52b5
Daniel Zalewski <dazzadman03@gmail.com>
2026-09-25 00:55:20 +1000

    shelving this project until i know how to code again

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\ClubController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+
+    Route::inertia('clubs/create', 'clubs/create')->name('clubs.create');
+    Route::post('clubs', [ClubController::class, 'store'])->name('clubs.store');
 });
 
 require __DIR__.'/settings.php';
